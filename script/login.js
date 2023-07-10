8b96f7fd901f4041d1999601107c3d79b218aa9d
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-07-10 13:21:18 -0300

    Adicionado return no botao continuar

---
+++ script/login.js
@@ -0,0 +1,15 @@
+// FUNÇÃO LOGAR NA PAG LOGIN
+
+function logar() {
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    // VALIDAÇÃO DO CAMPO E-MAIL E SENHA
+    if (email === "example@example.com" && senha.length >= 5) {
+        alert("Logado com Sucesso!!");
+        location.href =""; // PRECISAMOS COLOCAR O LINK DA PAGE HOME AQUI// 
+    } else {
+        alert("Usuário ou senha incorretos, tente novamente.");
+    }
+
+}
