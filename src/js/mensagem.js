e8890330cb995ec364c9304197f8c61d9dd64aa2
isaabellepontes <isaapontes010@gmail.com>
2023-07-12 17:56:48 -0300

    feat: colocando o redirecionar no botão de recuperação de senha

--- src/js/mensagem.js
+++ src/js/mensagem.js
@@ -1,9 +1,21 @@
 function exibirMensagem() {
+    // Definir a mensagem a ser exibida 
 var mensagem = "Email encaminhado com sucesso.";
 var paragrafo = document.createElement("p");
 paragrafo.textContent = mensagem;
+// Colocando a mensagem para aparecer na tela 
 var mensagemTela = document.getElementById("mensagem");
 mensagemTela.innerHTML = "";
 mensagemTela.appendChild(paragrafo);
 }
 
+function redirecionar() {
+    // Colocando a pagina de destino
+    var url = "login.html";
+    // Definir quantos milisegundos 
+    var atraso = 1000
+    // Redireciona para a pagina
+    setTimeout(function() {
+        window.location.href = url;
+    }, atraso);
+    }
