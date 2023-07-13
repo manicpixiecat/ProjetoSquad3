b1e23cf8336a46ee0e21a77f0324202a135ec898
Joa1zin <jvmm676@gmail.com>
2023-07-13 01:32:25 -0300

    fix: validação script registro

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,4 +1,4 @@
-const nome = document.getElementById('nome-registro')
+//const nome = document.getElementById('nome-registro')
 const rg = document.getElementById('RG-registro')
 const email = document.getElementById('email-registro')
 const senha = document.getElementById('senha-registro')
@@ -16,8 +16,6 @@ const complemento = document.getElementById('complemento-registro')
 
 //Botão de registro 
 const botao = document.getElementById('botao-registro')
-//
-const formulario = document.getElementById('formulario')
 
 //Função que consome a API e retorna um objeto contendo as informações do CEP
 function consultaCep(entrada) {
@@ -35,8 +33,6 @@ function insereDados(dados) {
     bairro.value = dados.bairro
     rua.value = dados.logradouro
 }
-
-
 
 //Função que limpa os campos do registro
 function limpaCampos() {
@@ -56,9 +52,6 @@ cep.addEventListener("blur", () => {
     }
 })
 
-//Evento que verifica o email 
-
-
 //Função que verifica se as senhas são iguais e troca a cor do campo se não cumprir os requisitos
 function validaSenha() {
     if (senha.value.length < 1 || confirmaSenha.value.length < 1) {
@@ -72,6 +65,7 @@ function validaSenha() {
     } else if (senha.value === confirmaSenha.value) {
         confirmaSenha.style.backgroundColor = "rgba(53, 218, 53, 0.76)"
         senha.style.backgroundColor = "rgba(53, 218, 53, 0.76)"
+        return true
     }
 }
 
@@ -106,33 +100,18 @@ confirmaSenha.addEventListener("blur", () => {
 
 //Evento verificar e validar os campos ao clicar no botão
 botao.addEventListener('click', () => {
-    if (validaRg() == false) {
-        alert('Insira um RG válido!')
-    }
     //Uso do jQuery para utilizar uma função de outro arquivo
     $(document).ready(function () {
         if (validarEmail(email.value)) {
             email.style.backgroundColor = "rgba(53, 218, 53, 0.76)"
-        } else {
+        }else{
             email.style.backgroundColor = "rgba(223, 64, 64, 0.76)"
-            alert("Insira um email válido!")
+            alert('Insira um email válido!')
         }
     });
-    if (validaSenha() == false) {
-        alert("As senhas devem coincidir e ter pelo menos 5 dígitos!")
+    if(validaRg() == true && validaSenha == true){
+        alert("Registro realizado!")
     }
-
-    
 })
 
 
-function exibirMensagem() {
-    // Definir a mensagem a ser exibida 
-var mensagem = "Email encaminhado com sucesso.";
-var paragrafo = document.createElement("p");
-paragrafo.textContent = mensagem;
-// Colocando a mensagem para aparecer na tela 
-var mensagemTela = document.getElementById("mensagem");
-mensagemTela.innerHTML = "";
-mensagemTela.appendChild(paragrafo);
-}
